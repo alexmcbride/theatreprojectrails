a148748e0bc1bed207204a3329ce38e4573b4d8a
Alex McBride <xandermcbride@gmail.com>
2017-02-09 18:24:39 +0000

    Added UsersController and assorted views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,29 @@ class User < ApplicationRecord
   def assign_default_role
     self.add_role(:member) if self.roles.blank?
   end
+
+  def clear_roles
+    self.remove_role :admin
+    self.remove_role :staff
+    self.remove_role :member
+  end
+
+  def set_role(role)
+    if role == 'Admin'
+      self.add_role :admin
+      self.add_role :staff
+    elsif role == 'Staff'
+      self.add_role :staff
+    end
+  end
+
+  def get_role
+    if self.has_role? :admin
+      return 'Admin'
+    end
+    if self.has_role? :staff
+      return 'Staff'
+    end
+    'Member'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :categories
   resources :posts
-  devise_for :users
+  resources :users
+
+  devise_for :users, path: 'auth'
   get 'home/index'
   get 'home/contact'
   get 'blog/:id', to: 'posts#index', as: 'category_posts'
